510280b0d22ab147b220767671a3c28c85d2289b
EXP <289065406@qq.com>
2019-08-08 00:27:02 +0800

    Update convert_cookie.sh

--- script/convert_cookie.sh
+++ script/convert_cookie.sh
@@ -2,16 +2,17 @@
 # 把 Cookie 参数转换成 Cookie 文件
 #-------------------------------------------------
 # 命令执行示例：
-# ./convert_cookie.sh -u USERNAME -p PASSWORD
+# ./convert_cookie.sh -c "DedeUserID=47225816;DedeUserID__ckMd5=fac9e6f28b02a888;SESSDATA=73ff9248%2C1567777289%2C821d2781;bili_jct=e8e5029cc3b89f92a712a243577a13c3;"
 #-------------------------------------------------
 
 # 命令参数定义
+export cookie=""             # -c: 完整 Cookie 串（k1=v1;k2=v2;...;kn=vn;），要求不能有空格
 export DedeUserID=""         # -i: [DedeUserID] Cookie 参数
 export DedeUserID__ckMd5=""  # -m: [DedeUserID__ckMd5] Cookie 参数
 export SESSDATA=""           # -s: [SESSDATA] Cookie 参数
 export bili_jct=""           # -b: [bili_jct] Cookie 参数
 export Expires="30"          # -e: [Expires] Cookie 有效天数（默认 30 天）
-export ctype="main"          # -t: 生成 Cookie 类型（main: 主号; mini: 小号; vest: 马甲）
+export ctype="mini"          # -t: 生成 Cookie 类型（main: 主号; mini: 小号; vest: 马甲）
 export cpath="./cookies"     # -o: 生成 Cookie 文件位置
 
 
@@ -20,12 +21,13 @@ usage()
 { 
   cat <<EOF
     -h                        This help message.
+    -c <cookie>               Full Cookie string: k1=v1;k2=v2;...;kn=vn;
     -i <DedeUserID>           Cookie parameter.
     -m <DedeUserID__ckMd5>    Cookie parameter.
     -s <SESSDATA>             Cookie parameter.
     -b <bili_jct>             Cookie parameter.
     -e <Expires>              Cookie expires days. (Default: 30)
-    -t <ctype>                Cookie type. (Eg: main, mini, vest)
+    -t <ctype>                Cookie type. (Eg: main, mini, vest. Default: mini)
     -o <cpath>                Save Cookies Directory. (Default: "./cookies")
 EOF
   exit 0 
@@ -36,10 +38,12 @@ EOF
 
 
 # 定义参数键和值
-set -- `getopt i:m:s:b:e:t:o: "$@"`
+set -- `getopt c:i:m:s:b:e:t:o: "$@"`
 while [ -n "$1" ]
 do
   case "$1" in 
+    -c) cookie="$2"
+        shift ;;
     -i) DedeUserID="$2"
         shift ;;
     -m) DedeUserID__ckMd5="$2"
@@ -59,8 +63,34 @@ do
 done
 
 
+if [ -n "${cookie}" ] ; then
+  OLD_IFS="$IFS"
+  IFS=";"
+  array=(${cookie})
+  IFS="$OLD_IFS"
+
+  for kv in ${array[@]}
+  do
+    if [[ ${kv} == DedeUserID__ckMd5* ]]; then
+      DedeUserID__ckMd5=${kv#*=}
+
+    elif [[ ${kv} == DedeUserID* ]] ; then
+      DedeUserID=${kv#*=}
+
+    elif [[ ${kv} == SESSDATA* ]] ; then
+      SESSDATA=${kv#*=}
+
+    elif [[ ${kv} == bili_jct* ]] ; then
+      bili_jct=${kv#*=}
+
+    fi
+  done
+fi
+
+
 Expires=`date -d "-${Expires} days ago" "+%a, %d %b %Y %T GMT+08:00"`
 echo "---------- Input Params ----------"
+echo "cookie = ${cookie}"
 echo "DedeUserID = ${DedeUserID}"
 echo "DedeUserID__ckMd5 = ${DedeUserID__ckMd5}"
 echo "SESSDATA = ${SESSDATA}"
